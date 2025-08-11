76a609f907c0aaa49f133797c6879da279928857
lindson25 <lindson_lucas@live.com>
2025-08-11 16:58:13 -0300

    Initial commit

---
+++ src/main/java/com/projects/petize/PetizeApplication.java
@@ -0,0 +1,13 @@
+package com.projects.petize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/petize/PetizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.petize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
